8a5df22150a25657ff4f432b368e64b08544e950
Odyssey-94 <82617020+Odyssey-94@users.noreply.github.com>
2022-03-08 11:22:54 -0800

    Added dependencies and CountryRestController to handle all dynamic AJAX requests that result from user interactions on the table of contents

--- coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/models/LocationStats.java
+++ coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/models/LocationStats.java
@@ -1,6 +1,17 @@
 package io.javabrains.coronavirustracker.models;
 
+// Data Table imports start
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// Data Table imports end
+@Getter
+@Setter
+@NoArgsConstructor
 public class LocationStats {
+
     private String state;
     private String country;
     private int latestTotalCases;
